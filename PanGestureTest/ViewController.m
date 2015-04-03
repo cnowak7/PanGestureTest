103e84e393a85540d94a8cf582014fcab0de72db
Chris Nowak <cnowak7@Chriss-MacBook-Air.local>
2015-04-03 14:42:58 -0500

    Initial Commit

---
+++ PanGestureTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PanGestureTest
+//
+//  Created by Chris Nowak on 4/3/15.
+//  Copyright (c) 2015 Beakon Labs, LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
